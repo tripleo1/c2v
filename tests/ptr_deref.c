d621d9064032a7104e6d4471a9ebc8a501afee61
Delyan Angelov <delian66@gmail.com>
2026-02-05 16:36:23 +0200

    add more simple cases to tests/ptr_deref.c

--- tests/ptr_deref.c
+++ tests/ptr_deref.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 
 void test_ptr_ops(char **endptr, int *value) {
-    // Reading from dereferenced pointer
-    if (*endptr != NULL) {
-        // Assigning to dereferenced pointer
-        *value = 42;
-    }
+  // Reading from dereferenced pointer
+  if (*endptr != NULL) {
+    // Assigning to dereferenced pointer
+    *value = 42;
+  }
+
+  int abc = 123;
+  int *pabc = &abc;
+  *value = *pabc;
 }
