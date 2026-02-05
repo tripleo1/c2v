63466bda13f6b422858eb119064967ac3a4872ad
Delyan Angelov <delian66@gmail.com>
2026-02-05 17:01:43 +0200

    ignore generated .v files under tests/

--- tests/ptr_deref.c
+++ tests/ptr_deref.c
@@ -19,12 +19,14 @@ void test_ptr_ops(char **endptr, int *value) {
   int frac = 5;
   int fracstep = 2;
   int FRACBITS = 2;
-  int *dest;
+  int *dest1;
   int *dest2;
+  int *dest3;
   do {
-     *dest2 = *dest = dc_colormap[dc_source[(frac >> FRACBITS) & 127]];
-     dest += 320;
+     *dest3 = *dest2 = *dest1 = dc_colormap[dc_source[(frac >> FRACBITS) & 127]];
+     dest1 += 320;
      dest2 += 320;
+     dest3 += 320;
      frac += fracstep;
   } while (count--);
 
