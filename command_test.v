ace5f45bbbc11e16df8790a163bba79fa4b520c8
Delyan Angelov <delian66@gmail.com>
2026-02-05 19:26:32 +0200

    ensure that the .md files are kept formatted too

--- command_test.v
+++ command_test.v
@@ -10,6 +10,12 @@ fn test_verify_formatting_of_source_code() {
 	println('> source code is formatted, good')
 }
 
+fn test_verify_formatting_of_markdown_docs() {
+	res := os.system('${os.quoted_path(@VEXE)} check-md .')
+	assert res == 0
+	println('> markdown documentation is formatted, good')
+}
+
 fn test_run_tests() {
 	res := os.system('${os.quoted_path(@VEXE)} tests/run_tests.vsh')
 	assert res == 0
